8e72d7aa4ed03aa155f4fa62f1af61aca3f29920
Ryan Garner <rpgarner22@gmail.com>
2022-05-10 14:00:50 -0600

    pages and component files made

---
+++ ntrest-clientapp/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+const Navbar = (props) => {
+
+    return (
+        <header>
+            <nav>
+                <p>test</p>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
