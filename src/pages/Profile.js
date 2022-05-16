dfe7e170293c2c78633d745e9407fe08bc3d45f4
Ryan Garner <rpgarner22@gmail.com>
2022-05-16 07:46:30 -0600

    cleaned up some styling real quick

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,19 @@
+
+
 const Profile = () => {
   return (
+      <div>
     <div className="feed">
       <div className="allEvents">
-        <h1 className="note">Trips</h1>
+        <h1 className="note">Do it again</h1>
       </div>
     </div>
+    <div className="feed">
+    <div className="allEvents">
+      <h1 className="note">Favorites</h1>
+    </div>
+  </div>
+  </div>
   );
 };
 
